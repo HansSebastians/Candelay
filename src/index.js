53bd1ed6bff941276684f9646cafe2b08011f35a
Hands Sabastian <email_github_kamu>
2026-08-31 14:58:07 +0700

    Add Base Routing

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Candelay">
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/feature" element={<FeaturePage />} />
